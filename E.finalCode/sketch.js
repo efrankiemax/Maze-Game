69ffa1716c665a0c58ead9f4a702fa0bad56b133
Frankie <efrankiemax@gmail.com>
2019-01-01 18:57:26 -0800

    1.1.19

--- E.finalCode/sketch.js
+++ E.finalCode/sketch.js
@@ -1,22 +1,98 @@
-
 let x;
 let y;
 let w;
+let p=10;
+
 function setup(){
-createCanvas(windowWidth,windowHeight);
-background(250);
+	createCanvas(windowWidth,windowHeight);
+	background(250);
+		// let z = new player(windowWidth/2,windowHeight/2);
+		// players.push(z);
+		// console.log(players);
 }
 
 function draw(){
 background(255);
-	fill(255,0,0);
-	let x=windowWidth/2-100;
-	let y=0;
-	let w=200;
-	rect(x,y,w,w);
-	if(mouseX>=x && mouseX<=x+w && mouseY>=y && mouseY<=y+w){
-		fill(0,255,0);
+let x=windowWidth/2-100;
+let y=0;
+let w=200;
+let p = 10;
+	fill(0,255,0);
 		rect(x,y,w,w);
-		print('I work')
-	}
+		if(mouseX>=x && mouseX<=x+w && mouseY>=y && mouseY<=y+w){
+			print('I work')
+			fill(255,0,0);
+				rect(x,y,w,w);
+			p = p-1
+		}
+	// loosePoints();
+	// for (let i = 0; i < players.length; i++) {
+	// 	players[i].drawPlayer();
+  //   players[i].movePlayer();
+	// 	players[i].diePlayer();
+  // }
+fill(255)
+rect(0,0,300,150)
+textSize(100);
+fill(0);
+text(p,50,100);
 }
+
+
+// class player {
+// 	constructor(x,y,w){ //every ball needs an x value and a y value
+// 		this.x = x;
+//   	this.y = y;
+// 		this.w = w;
+// 	}
+//
+// 	drawPlayer(){  // draw a ball on the screen at x,y
+// 		stroke(0);
+// 		strokeWeight(1);
+//     fill(0,0,255);
+// 		ellipse(this.x,this.y,this.w,this.w);
+// 		if (this.w >= 50 && keyIsDown(189)){
+// 			this.w = .95*this.w;
+// 		}
+// 	}
+//
+// 	// movePlayer(){
+// 	// 	if (keyIsDown(RIGHT_ARROW)) {
+// 	// 		this.x = this.x +10;
+// 	// 	}
+//  //
+// 	// 	if (keyIsDown(LEFT_ARROW)) {
+// 	// 		this.x = this.x -10;
+// 	// 	}
+//  //
+// 	// 	if(keyIsDown(UP_ARROW)){
+// 	// 		this.y = this.y -10;
+// 	// 	}
+//  //
+// 	// 	if(keyIsDown(DOWN_ARROW)){
+// 	// 		this.y = this.y +10;
+// 	// 	}
+//  //  }
+//  // diePlayer(){ //this is not working. I want it to create a white screen that says "you loose" when the player comes into contact with a line, right now it is not reacting at all.
+// 	//  if(b <= this.x+this.w && b>=this.x && this.y+this.w >= 0 && this.y+this.w <= d){
+// 	// 	 noStroke();
+// 	// 	 fill(255);
+// 	// 	 		rect(0,0,windowWidth,windowHeight);
+// 	// 	push();
+// 	// 		scale(5.0);
+// 	// 		fill(0);
+// 	// 			text('YOU LOOSE!',windowWidth/2,windowHeight/2);//the screen goes white, but teh text does not appear
+// 	// 	pop();
+// 	// 	}
+// 	// }
+// }
+
+// function loosePoints(){
+// 	let p = 10;
+// 	if(mouseX>=x && mouseX<=x+w && mouseY>=y && mouseY<=y+w){
+// 		print('I work')
+// 		fill(255,0,0);
+// 			rect(x,y,w,w);
+// 		p = p-1
+// 	}
+// }
